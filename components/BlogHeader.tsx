90b420b41bb43f6aa492bb202098825f1491851e
Brattus <olemartinbratteberg@gmail.com>
2024-02-02 22:34:43 +0100

    update

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -15,12 +15,12 @@ export default function BlogHeader({
   switch (level) {
     case 1:
       return (
-        <header className="mb-10 mt-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
-          <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
+        <header className="mb-10 mt-16 flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
+          <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl ">
             {title}
           </h1>
           <h4
-            className={`mt-5 text-center text-lg md:pl-8 md:text-left ${styles.portableText}`}
+            className={`mt-5 text-center text-lg md:pl-8 md:text-left md:max-w-lg ${styles.portableText}`}
           >
             <PortableText value={description} />
           </h4>
