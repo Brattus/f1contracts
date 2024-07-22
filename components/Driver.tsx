48230762c1e124525ed7630c2872fca9057e7213
Ole-Martin Bratteberg <olemartinbratteberg@gmail.com>
2024-07-22 14:43:14 +0200

    Update Driver.tsx

--- components/Driver.tsx
+++ components/Driver.tsx
@@ -2,28 +2,37 @@ import React from 'react';
 
 const Driver = (props) => {
     const thisYear = new Date().getFullYear();
-    const calculateYear = thisYear - 1;
-    const maxEndYear = props.maxEndYear;
+const calculateYear = thisYear - 1;
+const maxEndYear = props.maxEndYear;
 
-    const driver = props.driver;
-    const currentContract = driver.contracts.find(contract => contract.startYear <= thisYear && contract.endYear >= thisYear);
-    //latest contract  
-    const thisDriversMaxEndYear = driver.contracts[0].endYear;
-    const percentOfMaxEndYear = Math.round(((thisDriversMaxEndYear - calculateYear) / (maxEndYear - calculateYear)) * 100);
+const driver = props.driver;
+let currentContract = driver.contracts.find(contract => contract.startYear <= thisYear && contract.endYear >= thisYear);
 
-    // add width to each contract
-    const contracts = driver.contracts.map((contract) => {
-        let width = 100;
+// If currentContract is null, get the first contract
+if (!currentContract) {
+    currentContract = driver.contracts[0];
+}
 
-        const startYear = contract.startYear < thisYear ? thisYear : contract.startYear;
-        const numberOfYears = contract.endYear >= thisYear ? contract.endYear - startYear + 1 : 0;
-        width = (numberOfYears / (maxEndYear - thisYear + 1)) * 100;
+// Latest contract  
+const thisDriversMaxEndYear = driver.contracts[0].endYear;
 
-        return {
-            ...contract,
-            width: width,
-        };
-    });
+// Add width to each contract
+const contracts = driver.contracts.map((contract, index) => {
+    let width = 100;
+    let marginLeft = 0;
+
+    const startYear = contract.startYear < thisYear ? thisYear : contract.startYear;
+    const numberOfYears = contract.endYear >= thisYear ? contract.endYear - startYear + 1 : 0;
+    width = (numberOfYears / (maxEndYear - thisYear + 1)) * 100;
+
+   
+
+    return {
+        ...contract,
+        width: width,
+        marginLeft: driver.number == 99 ? 20 : 0,
+    };
+});
 
     // filter contracts and remove the ones that are not active
     const activeContracts = contracts.filter(contract => thisYear <= contract.endYear);
@@ -40,22 +49,18 @@ const Driver = (props) => {
 
     return (
         <div>
-            <h2>{driver.name} {'(' + driver.number + ')'} - {currentContract.team.name}</h2>
+            <h2>{driver.name} {'(' + driver.number + ')'} - {currentContract?.team.name}</h2>
 
             <div className="">
                 <ul className='flex'>
                     {activeContracts.map((contract, index, self) => (
-                        <li key={contract._key} className={`flex justify-end items-center space-x-2 ${contract._key === self[self.length - 1]._key ? 'rounded-r-full' : ''}`} style={{ width: contract.width + '%', backgroundColor: '#' + contract.team.color, color: getTextColor(contract.team.color) }}>
+                        <li key={contract._key} className={`flex justify-end items-center space-x-2 ${contract._key === self[self.length - 1]._key ? 'rounded-r-full' : ''}`} style={{ width: contract.width + '%', backgroundColor: '#' + contract.team.color, color: getTextColor(contract.team.color), marginLeft: contract.marginLeft + "%" }}>
                             <div className="pr-4">{contract.endYear}</div>
                         </li>
                     ))}
                 </ul>
                 <div className="text-sm opacity-50">{driver.comment}</div>
             </div>
-
-            {/* <div style={{ width: percentOfMaxEndYear + '%' }}>
-                <div className="h-3">{percentOfMaxEndYear}</div>
-            </div> */}
 
 
 
